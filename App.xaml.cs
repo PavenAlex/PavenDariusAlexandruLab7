ac3f7c0071b6e0307d28cbd81ea4ffe5e16ea1d9
PavenAlex <alexpaven@yahoo.com>
2024-11-26 17:06:05 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace PavenDariusAlexandruLab7
+﻿using System;
+using PavenDariusAlexandruLab7.Data;
+using System.IO;
+
+
+namespace PavenDariusAlexandruLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
